fc121f27d2b2321cd0d6a9c42f063247a3ee32a5
marcusdoverhed <marcusdov@hotmail.com>
2022-06-30 16:43:52 +0200

    färdig

--- API.py
+++ API.py
@@ -1,11 +1,13 @@
 import requests
 
 def get_phone(name):
-    r = requests.get('http://localhost:5000/api?action=phone&name=Urban')
+    r = requests.get(f'http://localhost:5000/api?action=phone&name={name}')
     name = r.text
-    return name
-    
+    return name    
+
 def get_name(phone):
-    r = requests.get('http://localhost:5000/api?action=name&phone=0435-4355438')
+    r = requests.get(f'http://localhost:5000/api?action=name&phone={phone}')
     phone = r.text
     return phone
+    
+    
